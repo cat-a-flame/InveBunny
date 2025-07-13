a228741a17189d06e5cae7e5f76de1f2d9f52ad5
Stefania Peter <1927103+cat-a-flame@users.noreply.github.com>
2025-07-13 15:46:07 +0200

    Loading aniamtion

--- src/components/PageLoader/pageLoader.tsx
+++ src/components/PageLoader/pageLoader.tsx
@@ -2,9 +2,17 @@ import styles from './pageLoader.module.css';
 
 export function PageLoader() {
     return (
-        <div className={styles.container}>
-            <div className={styles.spinner}></div>
-            <span>Loading…</span>
+        <div className={styles.loading}>
+            <div className={styles.header}>
+                <div className={styles.title}></div>
+                <div className={styles.button}></div>
+            </div>
+            <div className={styles.row}></div>
+            <div className={styles.row}></div>
+            <div className={styles.row}></div>
+            <div className={styles.row}></div>
+            <div className={styles.row}></div>
+            <div className={styles.row}></div>
         </div>
     );
 }
